0ccc8865cb3abdf25f95822a0e6f120128feab60
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-06-21 15:35:59 +0530

    32 working with state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './App.css';
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 
-const App = () => {
-  const courseGoals=[
-    {id: 'cg1', text:'Finish the course'},
+const App = () => { 
+  const [courseGoals, setCourseGoals] = useState([    //this is the initial state
+    {id: 'cg1', text:'Finish the course'},  //courseGoals holds the current state and setCourseGoals hods a function which can be used to update the state
     {id: 'cg2', text:'Learn all  about the course Main topic'},
     {id: 'cg3', text:'Help our students in the course Q&A'}
-  ];
+  ]);
 
   const addNewGoalHandler = newGoal => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
+    setCourseGoals(courseGoals.concat(newGoal));
   };
   return (<div className="course-goals">
     <h2>Course Goals</h2>
